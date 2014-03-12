1f4792e56e5a93eba3f5dbb3f328698d433d31f3
Xsan23 <xavier_san@outlook.com>
2014-03-12 12:53:19 -0400

    Uploaded Project
    This is my first committed to GitHub

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Producto_Cartesiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_A = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result_Box = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_A
+            // 
+            this.label_A.AutoSize = true;
+            this.label_A.BackColor = System.Drawing.Color.Transparent;
+            this.label_A.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label_A.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_A.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label_A.Location = new System.Drawing.Point(197, 54);
+            this.label_A.Name = "label_A";
+            this.label_A.Size = new System.Drawing.Size(19, 19);
+            this.label_A.TabIndex = 0;
+            this.label_A.Text = "A";
+            this.label_A.Click += new System.EventHandler(this.label_A_Click);
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.BackColor = System.Drawing.Color.Transparent;
+            this.labelB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelB.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.labelB.Location = new System.Drawing.Point(300, 54);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(17, 19);
+            this.labelB.TabIndex = 2;
+            this.labelB.Text = "B";
+            this.labelB.Click += new System.EventHandler(this.labelB_Click);
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.BackColor = System.Drawing.Color.Transparent;
+            this.labelC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelC.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.labelC.Location = new System.Drawing.Point(409, 54);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(17, 19);
+            this.labelC.TabIndex = 4;
+            this.labelC.Text = "C";
+            this.labelC.Click += new System.EventHandler(this.labelC_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxA.Location = new System.Drawing.Point(179, 103);
+            this.textBoxA.MaxLength = 3;
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(84, 20);
+            this.textBoxA.TabIndex = 9;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxB.Location = new System.Drawing.Point(269, 103);
+            this.textBoxB.MaxLength = 3;
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(84, 20);
+            this.textBoxB.TabIndex = 10;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.BackColor = System.Drawing.Color.White;
+            this.textBoxC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxC.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxC.Location = new System.Drawing.Point(359, 103);
+            this.textBoxC.MaxLength = 3;
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(84, 20);
+            this.textBoxC.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(269, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Solución";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result_Box
+            // 
+            this.result_Box.BackColor = System.Drawing.Color.White;
+            this.result_Box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result_Box.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_Box.Location = new System.Drawing.Point(0, 201);
+            this.result_Box.Multiline = true;
+            this.result_Box.Name = "result_Box";
+            this.result_Box.ReadOnly = true;
+            this.result_Box.Size = new System.Drawing.Size(644, 120);
+            this.result_Box.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button2.Location = new System.Drawing.Point(226, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Selección automática";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(452, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 117);
+            this.label1.TabIndex = 17;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(644, 178);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.result_Box);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.labelC);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.label_A);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Producto Cartesiano";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_A;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox result_Box;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Producto_Cartesiano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string valoresCasillaA = textBoxA.Text;
+            string valoresCasillaB = textBoxB.Text;
+            string valoresCasillaC = textBoxC.Text;
+            string[] arreglo1 = new string[textBoxA.TextLength], arreglo2 = new string[textBoxB.TextLength], arreglo3 = new string[textBoxC.TextLength];
+            int cantidadDeNumeros = (textBoxA.TextLength + textBoxB.TextLength + textBoxC.TextLength);
+
+            for (int i = 0; i < valoresCasillaA.Length; i++)
+            {
+                string value = valoresCasillaA.Substring(i, 1);
+                arreglo1[i] = value;
+            }
+
+            for (int j = 0; j < valoresCasillaB.Length; j++)
+            {
+                string value = valoresCasillaB.Substring(j, 1);
+                arreglo2[j] = value;
+            }
+
+            for (int k = 0; k < valoresCasillaC.Length; k++)
+            {
+                string value = valoresCasillaC.Substring(k, 1);
+                arreglo3[k] = value;
+            }
+
+
+            //**********************************************************************************************************
+            // **********************************Calculo del producto cartesiano**************************************** 
+            
+            
+            if (textBoxA.Text == "" || textBoxB.Text == "" || textBoxC.Text == "")
+            {
+                MessageBox.Show("Complete todos los encasillados para proceder a calcular el Producto cartesiano", "Incompleto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (button1.Text == "Borrar")
+            {
+                this.Height = 229;
+                result_Box.Text = null;
+                button1.Text = "Solución";
+                textBoxA.Text = ""; 
+                textBoxB.Text = "";
+                textBoxC.Text = "";
+            }
+            else if(button1.Text == "Solución")
+            {
+                result_Box.Text = "El Producto Cartesiano A x B es: { ";
+
+                foreach (var item in arreglo1)
+                {
+                    foreach (var valores in arreglo2)
+                    {
+                        result_Box.Text += "(" + item + ", " + valores + "), ";
+                    }
+                }
+
+                result_Box.Text += "}\n\n\nEl Producto Cartesiano B x A es: {";
+
+
+                foreach (var item in arreglo2)
+                {
+                    foreach (var valores in arreglo1)
+                    {
+                        result_Box.Text += "(" + item + ", " + valores + "), ";
+                    }
+                }
+
+                result_Box.Text += "}\n\n\nEl Producto Cartesiano A x B x C es: { ";
+
+                foreach (var item in arreglo1)
+                {
+                    foreach (var valores in arreglo2)
+                    {
+                        foreach (var valoresFinales in arreglo3)
+                        {
+                            result_Box.Text += "(" + item + ", " + valores + ", " + valoresFinales + "), ";
+                        }
+                    }
+
+                }
+
+                result_Box.Text += "}\n\n\nEl Producto Cartesiano C x B x A es: { ";
+
+                foreach (var item in arreglo3)
+                {
+                    foreach (var valores in arreglo2)
+                    {
+                        foreach (var valoresFinales in arreglo1)
+                        {
+                            result_Box.Text += "(" + item + ", " + valores + ", " + valoresFinales + "), ";
+                        }
+                    }
+
+                }
+
+                this.Height = 349;
+                button1.Text = "Borrar";
+            }
+            }
+        
+
+        private void label_A_Click(object sender, EventArgs e)
+        {
+            Random rand1 = new Random();
+
+            int number = rand1.Next(1000);
+            textBoxA.Text = number.ToString();
+        }
+
+        private void labelB_Click(object sender, EventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random rand2 = new Random();
+            char ch;
+
+            for (int i = 0; i < 2; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * rand2.NextDouble() + 65)));
+                builder.Append(ch);
+                textBoxB.Text = builder.ToString();
+                textBoxC.Text = builder.ToString().ToLower();
+            }
+}
+
+       /* private void borrar_btn_Click(object sender, EventArgs e)
+        {
+            this.Height = 229;
+            result_Box.Text = null;
+            this.borrar_btn.Visible = false;
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Llena la casilla A
+            Random rand1 = new Random();
+
+            int number = rand1.Next(1000);
+            textBoxA.Text = number.ToString();
+
+            //Llena la casilla B y C
+            StringBuilder builder = new StringBuilder();
+            Random rand2 = new Random();
+            char ch;
+
+            for (int i = 0; i < 2; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * rand2.NextDouble() + 65)));
+                builder.Append(ch);
+                textBoxB.Text = builder.ToString();
+                textBoxC.Text = builder.ToString().ToLower(); // Esta linea llena la casilla C
+            }
+        }
+
+        private void labelC_Click(object sender, EventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random rand2 = new Random();
+            char ch;
+
+            for (int i = 0; i < 2; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * rand2.NextDouble() + 65)));
+                builder.Append(ch);
+                textBoxC.Text = builder.ToString().ToLower(); // Esta linea llena la casilla C
+            }
+        }
+    }
+}
